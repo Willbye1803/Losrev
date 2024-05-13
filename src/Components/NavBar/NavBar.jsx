89a09eee063ea24d6e5f59c54cc8edc0a70676a5
cesar <megakuler993@gmail.com>
2024-05-13 10:57:47 -0600

    commit

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import Logo from "/logoLetters.png"
 
-
 const NavBar = () => {
   return (
     <section className='flex justify-between items-center bg-zinc-300 bg-opacity-10 absolute w-full'>
-      <img src={Logo} alt="" className='pl-10 w-1/4'/>
-        <ul className='flex justify-between w-4/12 pr-10'>
-          <li className='bg-red-900 p-3 rounded-full text-white w-3/12 text-center items-center uppercase'>Quienes Somos</li>
-          <li className='bg-red-900 p-3 rounded-full text-white w-3/12 text-center items-center uppercase'>Menu</li>
-          <li className='bg-red-900 p-3 rounded-full text-white w-3/12 text-center items-center uppercase'>Ubicacion</li>
-        </ul>
+      <div className="flex items-center">
+        <img src={Logo} alt="" className='pl-4 md:pl-10 w-3/4 md:w-auto h-auto max-h-12'/>
+      </div>
+      <ul className='flex md:hidden justify-center w-full'> 
+        <li className='bg-red-900 p-3 rounded-full text-white text-center uppercase mx-2'>Quiénes Somos</li>
+        <li className='bg-red-900 p-3 rounded-full text-white text-center uppercase mx-2'>Menú</li>
+        <li className='bg-red-900 p-3 rounded-full text-white text-center uppercase mx-2'>Ubicación</li>
+      </ul>
+      <ul className='hidden md:flex justify-center w-1/2 md:w-auto'> 
+        <li className='bg-red-900 p-3 rounded-full text-white text-center uppercase mx-2'>Quiénes Somos</li>
+        <li className='bg-red-900 p-3 rounded-full text-white text-center uppercase mx-2'>Menú</li>
+        <li className='bg-red-900 p-3 rounded-full text-white text-center uppercase mx-2'>Ubicación</li>
+      </ul>
     </section>
   )
 }
