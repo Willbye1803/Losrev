fa98e29fdff4e7c268236c30416b68b049dd2d91
cesar <megakuler993@gmail.com>
2024-05-14 17:50:03 -0600

    Login Agregado

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from './Pages/Home'
 import Menu from './Pages/Menu'
 import Location from './Pages/Location'
 import NavBar from './Components/NavBar/NavBar'
+import Login from './Pages/Login'
 
 
 
@@ -20,6 +21,7 @@ function App() {
         <Route path="/" element={<Home />}/>
         <Route path="/menu" element={<Menu />}/>
         <Route path="/location" element={<Location />}/>
+        <Route path="/Login" element={<Login/>}/>
       </Routes>
     </HashRouter>
 
