6e10b7a801db6329b91f6f556b837e9da97c8118
cesar <megakuler993@gmail.com>
2024-05-15 18:47:27 -0600

    Login corregido

--- src/Components/LoginPage/LoginPage.jsx
+++ src/Components/LoginPage/LoginPage.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import LogoPrincipal from "../LoginPage/logo.jpeg";
+import LogoPrincipal from "../LoginPage/logoHero.png";
+
 
 const LoginPage = () => {
   const [condicionesAceptadas, setCondicionesAceptadas] = useState(false);
@@ -10,31 +11,33 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="bg-red-700 min-h-screen flex items-center justify-center">
-      <div className="bg-white p-8 rounded-lg shadow-md w-80">
+    <div className="bg-red-800 min-h-screen flex items-center justify-center">
+      <div className="bg-white p-10 rounded-lg shadow-md w-160">
         <div className="flex justify-center mb-8">
-          <img src={LogoPrincipal} alt="Logo" className="h-20" />
+          <img src={LogoPrincipal} alt="Logo" className="h-28" />
         </div>
-        <h1 className="text-xl font-semibold mb-6">Iniciar Sesión</h1>
+        <h1 className="text-xl font-semibold mb-6 text-center">Iniciar Sesión</h1>
         <div className="text-center">
           <div className="mb-4">
             <input
               type="text"
-              className="login-field"
-              placeholder="Usuario"
+              className="login-field  p-2 rounded w-full" 
+              placeholder="Nombre de usuario "
               id="login-name"
             />
           </div>
           <div className="mb-4">
             <input
               type="password"
-              className="login-field"
+              className="login-field p-2 rounded w-full"
               placeholder="Contraseña"
               id="login-pass"
             />
           </div>
-          <button className="btn btn-primary btn-large btn-block mb-4">
-            Iniciar Sesión
+          <button className="btn btn-primary btn-large btn-block mb-4 bg-red-800 border-2 border-transparent
+           text-white text-base font-bold py-2 px-4 transition duration-300 ease-in-out
+            hover:bg-red-600 focus:outline-none focus:shadow-outline w-64 rounded-md"> {/* Aquí añadimos la clase 'rounded-md' para hacer el botón ligeramente redondeado */}
+            Ingresar
           </button>
           <label htmlFor="condiciones" className="block mb-4">
             <input
